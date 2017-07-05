4a55fcdb99571952d2ad7452163622ed53b43596
Joseph Hyatt <jhyatt1017@gmail.com>
2017-07-05 12:07:32 -0700

    file structure

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+require './config/environment'
+require 'sinatra/flash'
+
+class ApplicationController < Sinatra::Base
+
+  # enable sessions
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'movies_are_awesome'
+    register Sinatra::Flash
+  end
+
+  # Homepage
+  get '/' do
+    erb :index
+  end
+
+end
