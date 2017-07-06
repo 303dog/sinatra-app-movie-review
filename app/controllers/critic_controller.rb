d6fd6054ae7c6eecea7e5aae264860fc916e849d
Joseph Hyatt <jhyatt1017@gmail.com>
2017-07-05 17:04:49 -0700

    Update critic_controller.rb
    Sign up method

--- app/controllers/critic_controller.rb
+++ app/controllers/critic_controller.rb
@@ -1,4 +1,13 @@
 class CriticController < ApplicationController
 
+  # SIGN UP
+    get '/signup' do
+      if is_logged_in?
+        flash[:message] = "You were already logged in. Here are your reviews."
+        redirect to '/reviews'
+      else
+        erb :'critic/create_critic'
+      end
+    end
 
 end
