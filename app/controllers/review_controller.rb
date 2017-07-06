ddd14be01fa44e2b45e6086432436263bdc50574
Joseph Hyatt <jhyatt1017@gmail.com>
2017-07-05 17:01:24 -0700

    Update review_controller.rb
    create method and  takes you to the create a movie review page

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,4 +1,13 @@
 class ReviewController < ApplicationController
 
+  # Create
+  get '/reviews/new' do # takes you to the create a movie review page
+    if is_logged_in?
+      erb :'reviews/new'
+    else
+      flash[:message] = "Looks like you weren't logged in yet. Please log in below."
+      redirect to '/login'
+    end
+  end
 
 end
