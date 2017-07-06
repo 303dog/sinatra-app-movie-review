53918896bf809a023366ee9018773bd311927511
Joseph Hyatt <jhyatt1017@gmail.com>
2017-07-05 18:00:37 -0700

    Update critic_controller.rb

--- app/controllers/critic_controller.rb
+++ app/controllers/critic_controller.rb
@@ -9,5 +9,20 @@ class CriticController < ApplicationController
         erb :'critic/create_critic'
       end
     end
+  
+    post '/signup' do
+      if is_logged_in?
+        flash[:message] = "You were already logged in. Here are your reviews."
+        redirect to '/reviews'
+      elsif params[:username] == "" || params[:password] == ""
+        flash[:message] = "In order to sign up for account, you must have both a username & a password. Please try again."
+        redirect to '/signup'
+      else
+        @critic = Critic.create(username: params[:username], password: params[:password])
+        @critic.save
+        session[:critic_id] = @critic.id 
+        redirect to '/reviews'
+      end
+    end
 
 end
